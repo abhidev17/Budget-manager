263b15d58ab57bf6c251f727d60f9e8ff6b5fbf0
abhidev17 <abhidevmohan3@gmail.com>
2026-07-03 14:56:51 +0530

    Fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig(() => ({
-  base: process.env.GITHUB_PAGES === 'true' ? '/Budget-manager/' : './',
+export default defineConfig({
+  base: '/Budget-manager/',
   plugins: [react()],
-}))
+})
